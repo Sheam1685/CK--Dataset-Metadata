9f283c6bf80be3cc90495d0226dc8dd55868635c
Sheam1685 <samia12676@gmail.com>
2024-06-08 11:49:36 +0600

    Update in image generation code.

--- Generate_Features_images.py
+++ Generate_Features_images.py
@@ -1,6 +1,14 @@
 from PIL import Image, ImageDraw
 import math
 
+
+# Choose emotion and mode from here. 
+# emotion = "happiness"
+# emotion = "surprise"
+# emotion = "anger"
+emotion = "sad"
+mode = "neutral"
+# mode = "emotional"
 def read_landmarks(file_path, supersample_factor=10):
     landmarks = []
     with open(file_path, 'r') as file:
@@ -20,8 +28,19 @@ def angle_between_points(point1, point2):
     # print("Degree ", degree)
     return degree
 
+def plot_landmarks(image, emotion, landmarks, width, height):
+    size = 30
+    draw = ImageDraw.Draw(image)
+    
+    for landmark in landmarks:
+        draw.ellipse([landmark[0] - size, landmark[1] - size, landmark[0] + size, landmark[1] + size], fill=(255, 0, 0, 255))
+    
+    image = image.resize((width, height), resample=Image.LANCZOS)
+    image.show()
+    image.save(f'{emotion}_landmarks.png')
+
 def plot_line_features(image, emotion, landmarks, line_features, width, height):
-    size = 20
+    size = 30
     draw = ImageDraw.Draw(image)
     
     for landmark in landmarks:
@@ -32,14 +51,17 @@ def plot_line_features(image, emotion, landmarks, line_features, width, height):
         point1 = tuple(landmarks[feature[0]-1])
         point2 = tuple(landmarks[feature[1]-1])
         # Draw double headed arrow
-        draw.line([point1, point2], fill=(0, 255, 0, 255), width=int(size*0.8))
+        draw.line([point1, point2], fill=(0, 255, 0, 255), width=int(size*0.9))
     
     image = image.resize((width, height), resample=Image.LANCZOS)
     image.show()
-    image.save(f'{emotion}_line_features.png')
+    if mode == "emotional":
+        image.save(f'{emotion}_line_features.png')
+    elif mode == "neutral":
+        image.save(f'{emotion}_line_features_neutral.png')
     
 def plot_angle_features(image, emotion, landmarks, angle_features, width, height):
-    size = 20
+    size = 30
     draw = ImageDraw.Draw(image)
     
     for landmark in landmarks:
@@ -52,12 +74,12 @@ def plot_angle_features(image, emotion, landmarks, angle_features, width, height
         point2 = tuple(landmarks[feature[1]-1])
         point3 = tuple(landmarks[feature[2]-1])
 
-        draw.line([point1, point2], fill=(0, 0, 255, 255), width=int(size*0.8))
-        draw.line([point2, point3], fill=(0, 0, 255, 255), width=int(size*0.8))
+        draw.line([point1, point2], fill=(0, 0, 255, 255), width=int(size*0.9))
+        draw.line([point2, point3], fill=(0, 0, 255, 255), width=int(size*0.9))
         
         # Draw the arc
         center = (point2[0], point2[1])  # The center is the point2
-        radius = 90
+        radius = 125
         start_angle = angle_between_points(point2, point1)
         end_angle = angle_between_points(point2, point3)
         
@@ -74,7 +96,10 @@ def plot_angle_features(image, emotion, landmarks, angle_features, width, height
         
     image = image.resize((width, height), resample=Image.LANCZOS)
     image.show()
-    image.save(f'{emotion}_angle_features.png')
+    if mode == "emotional":
+        image.save(f'{emotion}_angle_features.png')
+    elif mode == "neutral":
+        image.save(f'{emotion}_angle_features_neutral.png')
 
 def plot_features_on_image(image_path, landmarks, emotion,supersample_factor, line_features, angle_features):
     image = Image.open(image_path)
@@ -82,9 +107,11 @@ def plot_features_on_image(image_path, landmarks, emotion,supersample_factor, li
     image = image.convert("L")
     image = image.convert("RGBA")
     width, height = image.size
+    im = image.resize((width * supersample_factor, height * supersample_factor), resample=Image.LANCZOS)
     image_line = image.resize((width * supersample_factor, height * supersample_factor), resample=Image.LANCZOS)
     image_angle = image.resize((width * supersample_factor, height * supersample_factor), resample=Image.LANCZOS)
     
+    # plot_landmarks(im, emotion, landmarks, width, height)
     plot_line_features(image_line, emotion, landmarks, line_features, width, height)
     plot_angle_features(image_angle, emotion, landmarks, angle_features, width, height)
 
@@ -98,34 +125,45 @@ image_paths = {}
 landmarks = {}
 
 
-happiness_image_path = "C:\\Users\\samia\\Documents\\Thesis\\CK+\\EmotionBasedDataSet\\happiness\\S113\\S113_004_00000023.png"
-happiness_landmarks = read_landmarks("C:\\Users\\samia\\Documents\\Thesis\\CK+\\Landmarks\\S113\\004\\S113_004_00000023_landmarks.txt", supersample_factor)
+if mode == "emotional":
+    happiness_image_path = "C:\\Users\\samia\\Documents\\Thesis\\CK+\\EmotionBasedDataSet\\happiness\\S113\\S113_004_00000023.png"
+    happiness_landmarks = read_landmarks("C:\\Users\\samia\\Documents\\Thesis\\CK+\\Landmarks\\S113\\004\\S113_004_00000023_landmarks.txt", supersample_factor)
+elif mode == "neutral":
+    happiness_image_path = "C:\\Users\\samia\\Documents\\Thesis\\CK+\\EmotionBasedDataSet\\happiness\\S113\\S113_004_00000001.png"
+    happiness_landmarks = read_landmarks("C:\\Users\\samia\\Documents\\Thesis\\CK+\\Landmarks\\S113\\004\\S113_004_00000001_landmarks.txt", supersample_factor)
 
 image_paths["happiness"] = happiness_image_path
 landmarks["happiness"] = happiness_landmarks
 
-surprise_image_path = "C:\\Users\\samia\\Documents\\Thesis\\CK+\\EmotionBasedDataSet\\surprise\\S113\\S113_001_00000012.png"
-surprise_landmarks = read_landmarks("C:\\Users\\samia\\Documents\\Thesis\\CK+\\Landmarks\\S113\\001\\S113_001_00000012_landmarks.txt", supersample_factor)
+if mode == "emotional":
+    surprise_image_path = "C:\\Users\\samia\\Documents\\Thesis\\CK+\\EmotionBasedDataSet\\surprise\\S113\\S113_001_00000012.png"
+    surprise_landmarks = read_landmarks("C:\\Users\\samia\\Documents\\Thesis\\CK+\\Landmarks\\S113\\001\\S113_001_00000012_landmarks.txt", supersample_factor)
+elif mode == "neutral":
+    surprise_image_path = "C:\\Users\\samia\\Documents\\Thesis\\CK+\\EmotionBasedDataSet\\surprise\\S113\\S113_001_00000001.png"
+    surprise_landmarks = read_landmarks("C:\\Users\\samia\\Documents\\Thesis\\CK+\\Landmarks\\S113\\001\\S113_001_00000001_landmarks.txt", supersample_factor)
 
 image_paths["surprise"] = surprise_image_path
 landmarks["surprise"] = surprise_landmarks
 
-anger_image_path = "C:\\Users\\samia\\Documents\\Thesis\\CK+\\EmotionBasedDataSet\\anger\\S113\\S113_008_00000023.png"
-anger_landmarks = read_landmarks("C:\\Users\\samia\\Documents\\Thesis\\CK+\\Landmarks\\S113\\008\\S113_008_00000023_landmarks.txt", supersample_factor)
+if mode == "emotional":
+    anger_image_path = "C:\\Users\\samia\\Documents\\Thesis\\CK+\\EmotionBasedDataSet\\anger\\S113\\S113_008_00000023.png"
+    anger_landmarks = read_landmarks("C:\\Users\\samia\\Documents\\Thesis\\CK+\\Landmarks\\S113\\008\\S113_008_00000023_landmarks.txt", supersample_factor)
+elif mode == "neutral":
+    anger_image_path = "C:\\Users\\samia\\Documents\\Thesis\\CK+\\EmotionBasedDataSet\\anger\\S113\\S113_008_00000001.png"
+    anger_landmarks = read_landmarks("C:\\Users\\samia\\Documents\\Thesis\\CK+\\Landmarks\\S113\\008\\S113_008_00000001_landmarks.txt", supersample_factor)
 
 image_paths["anger"] = anger_image_path
 landmarks["anger"] = anger_landmarks
 
-sad_image_path = "C:\\Users\\samia\\Documents\\Thesis\\CK+\\EmotionBasedDataSet\\sadness\\S113\\S113_005_00000008.png"
-sad_landmarks = read_landmarks("C:\\Users\\samia\\Documents\\Thesis\\CK+\\Landmarks\\S113\\005\\S113_005_00000008_landmarks.txt", supersample_factor)
+if mode == "emotional":
+    sad_image_path = "C:\\Users\\samia\\Documents\\Thesis\\CK+\\EmotionBasedDataSet\\sadness\\S113\\S113_005_00000008.png"
+    sad_landmarks = read_landmarks("C:\\Users\\samia\\Documents\\Thesis\\CK+\\Landmarks\\S113\\005\\S113_005_00000008_landmarks.txt", supersample_factor)
+elif mode == "neutral":
+    sad_image_path = "C:\\Users\\samia\\Documents\\Thesis\\CK+\\EmotionBasedDataSet\\sadness\\S113\\S113_005_00000001.png"
+    sad_landmarks = read_landmarks("C:\\Users\\samia\\Documents\\Thesis\\CK+\\Landmarks\\S113\\005\\S113_005_00000001_landmarks.txt", supersample_factor)
 
 image_paths["sad"] = sad_image_path
 landmarks["sad"] = sad_landmarks
-
-
-
-
-emotion = "happiness"
 
 # Take features as input and plot them on the image
 
